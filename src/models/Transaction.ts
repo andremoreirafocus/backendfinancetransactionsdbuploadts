8445c46146d3f5ab719f9ce252a1fb5eff8f40f7
Andre Moreira <andre.moreira@focus-sys.com.br>
2020-12-29 02:59:28 -0300

    Models and migrations created

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,27 @@
+import { Column, CreateDateColumn, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import Category from "./Category";
+
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
-  category_id: string;
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id'})
+  category: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
